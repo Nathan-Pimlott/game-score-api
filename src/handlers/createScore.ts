192323ce9e0347207bd131435f2569b4c713c4dd
Nathan Pimlott <nathanpimlott@hotmail.co.uk>
2025-01-20 16:15:17 +0000

    Commented out create function and noted it will not be needed just yet

--- src/handlers/createScore.ts
+++ src/handlers/createScore.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
-import { IScore, IScoreToCreate } from '../types';
+// import { IScore } from '../types';
 
+// This still needs some thought. May need its own project.
 export async function createScore(req: Request, res: Response) {
-  const score: IScoreToCreate = req.body.score;
+  return res.status(201).send(req.body.score);
+  // const score: IScoreToCreate = req.body.score;
 
-  const createdScore: IScore = {
-    ...score,
-    id: '123',
-  };
+  // const createdScore: IScore = {
+  //   ...score,
+  //   id: '123',
+  // };
 
-  return res.status(201).send({ score: createdScore });
+  // return res.status(201).send({ score: createdScore });
 }
