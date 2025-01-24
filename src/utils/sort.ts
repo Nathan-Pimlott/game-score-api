2eefb58232970b6425cc290dd9caad8c7c8f1c1e
Nathan Pimlott <nathanpimlott@hotmail.co.uk>
2025-01-24 16:02:04 +0000

    Adding more unit tests

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,3 +1,5 @@
 export function sortAsc(arr: any[], propName: string) {
-  return arr.sort((a, b) => (a[propName] > b[propName] ? 1 : -1));
+  return arr.sort((a, b) =>
+    a[propName].toLowerCase() > b[propName].toLowerCase() ? 1 : -1
+  );
 }
